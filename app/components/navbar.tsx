5fda17c71fdbfcf0b063ec0a9c65f7e6b87cde4e
QuoteDev <gavinwinchell@gmail.com>
2024-06-12 00:21:00 -0600

    chore: Update navbar styles and button layout

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,15 +1,17 @@
 // app/components/navbar.js
-import React from 'react';
-import './navbar.css';
+import React from "react";
+import "./navbar.css";
 
 const Navbar = () => {
   return (
     <div className="navbarContainer">
-      <div className="navbarLogoBar">
-        {/* Your logo here */}
-      </div>
-      <div className="navbarButton">
-        {/* Your button here */}
+      <div className="navbarLogoBar">{/* Logo */}</div>
+      <div className="navbarButtonContainer">
+        <button className="navButton" color="primary">HOME</button>
+        <button className="navButton" color="primary">ABOUT</button>
+        <button className="navButton" color="primary">PRICING</button>
+        <button className="navButton" color="primary">SIGN UP</button>
+
       </div>
     </div>
   );
