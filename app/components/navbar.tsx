5e47d8a88318cf6a20d11259f2998e9757baae3d
QuoteDev <gavinwinchell@gmail.com>
2024-06-12 01:01:28 -0600

    Update navbar styles and button layout

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -5,13 +5,25 @@ import "./navbar.css";
 const Navbar = () => {
   return (
     <div className="navbarContainer">
-      <div className="navbarLogoBar">{/* Logo */}</div>
-      <div className="navbarButtonContainer">
-        <button className="navButton" color="primary">HOME</button>
-        <button className="navButton" color="primary">ABOUT</button>
-        <button className="navButton" color="primary">PRICING</button>
-        <button className="navButton" color="primary">SIGN UP</button>
+      <div className="navbarLogoBar">{
+      /* Logo */
+      <img src="images/FileMule-White_Logo.png" alt="Logo" className="navbarLogo" />
+        }
 
+      </div>
+      <div className="navbarButtonContainer">
+        <button className="navButton" color="primary">
+          HOME
+        </button>
+        <button className="navButton" color="primary">
+          ABOUT
+        </button>
+        <button className="navButton" color="primary">
+          PRICING
+        </button>
+        <button className="navButton" color="primary">
+          SIGN UP
+        </button>
       </div>
     </div>
   );
